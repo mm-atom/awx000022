d16fa26cf8337ab8beb94c710966a11eb528ded8
taoqf <tao_qiufeng@126.com>
2020-04-03 12:43:40 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import awx2 from '@mmstudio/awx000002';
 
-export default function awx22(param1: string, param2: string) {
+/**
+ * 设置local存储
+ * @param mm 固定参数
+ * @param key 键
+ * @param val 值(只能引用)
+ */
+export default function set<T>(mm: awx2, key: string, val: T) {
+	const local = mm.data.local as { [key: string]: unknown; };
+	local[key] = val;
 }
